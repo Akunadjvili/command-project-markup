3c11061ba5f287048e133e4ceec199ccbd844766
Volodymyr Davydenko <Akunadjvili@gmail.com>
2020-12-17 18:41:36 +0200

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -189,7 +189,7 @@ var reloadCSS = require('_css_loader');
 
 module.hot.dispose(reloadCSS);
 module.hot.accept(reloadCSS);
-},{"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
+},{"./..\\images\\manufacture\\manufacture-bg.png":[["manufacture-bg.ffb821b7.png","images/manufacture/manufacture-bg.png"],"images/manufacture/manufacture-bg.png"],"./..\\images\\svg\\bracket-right.svg":[["bracket-right.286db275.svg","images/svg/bracket-right.svg"],"images/svg/bracket-right.svg"],"_css_loader":"../node_modules/parcel-bundler/src/builtins/css-loader.js"}],"index.js":[function(require,module,exports) {
 "use strict";
 
 require("./sass/main.scss");
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "6230" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10983" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
