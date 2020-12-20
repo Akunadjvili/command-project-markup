3501b251671084f6ec4877074941b2750562af35
Akunadjvili <Akunadjvili@gmail.com>
2020-12-20 10:48:23 +0200

    Updates

--- slick.min.d8b8b7fa.js
+++ slick.min.d8b8b7fa.js
@@ -154,7 +154,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10040" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "10364" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
